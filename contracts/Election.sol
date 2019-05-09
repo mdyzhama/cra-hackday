49dbfe4c492b6ac980462adec18ef397ff4b3081
PypoliMoodys <Pierre-Yves.Poli@moodys.com>
2019-05-09 15:06:03 -0700

    Step 2 of the tutorial, modifying to account for new version of solidity

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,28 @@
 pragma solidity 0.5.0;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
     // Constructor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 }
